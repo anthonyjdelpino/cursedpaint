07456d12f1e3984e7db43e47b457cdb986a08d3b
anthonyjdelpino <57963375+anthonyjdelpino@users.noreply.github.com>
2020-12-29 18:43:37 +0000

    small changes
    add brushes, display color and brush type at top, prevent buttons from being overwritten

--- cursedpaint.py
+++ cursedpaint.py
@@ -15,18 +15,21 @@ def main(stdscr):
     stdscr.keypad(True)
     stdscr.clear()
     stdscr.refresh()
-    #if curses.has_colors(): stdscr.addstr(2,0, "has color")
+
     curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_BLUE, curses.COLOR_BLACK)
+    paintColor = 1
 
     stdscr.addstr(1,0, 'press q or click me to close', curses.color_pair(2))
     stdscr.addstr(2,0, 'RED', curses.color_pair(1))
     stdscr.addstr(2,10, 'GREEN', curses.color_pair(2))
     stdscr.addstr(2,20, 'BLUE', curses.color_pair(3))
+    brushTip = '@'
+    stdscr.addstr(3, 0, 'Brushes: # @ - + $')
     stdscr.refresh()
 
-    paintColor = 1
+    
 
     while True:
         event = stdscr.getch()
@@ -36,11 +39,21 @@ def main(stdscr):
             if mouseY == 1 and mouseX in range(0, 28): break
             if mouseY == 2:
                 if mouseX in range(0, 3): paintColor = 1
-                elif mouseX in range(10, 14): paintColor = 2
-                elif mouseX in range(20, 23): paintColor = 3
+                elif mouseX in range(10, 15): paintColor = 2
+                elif mouseX in range(20, 24): paintColor = 3
+                stdscr.addstr(0, 15, 'COLOR', curses.color_pair(paintColor))
+                continue
+            if mouseY == 3:
+                if mouseX == 9: brushTip = '#'
+                elif mouseX == 11: brushTip = '@'
+                elif mouseX == 13: brushTip = '-'
+                elif mouseX == 15: brushTip = '+'
+                elif mouseX == 17: brushTip = '$'
+                stdscr.addch(0, 25, brushTip)
+                continue
             stdscr.addstr(0,0, '            ')
             stdscr.addstr(0, 0, 'Y: {} X: {}'.format(mouseY, mouseX))
-            stdscr.addch(mouseY, mouseX, 'x', curses.color_pair(paintColor))
+            stdscr.addch(mouseY, mouseX, brushTip, curses.color_pair(paintColor))
             stdscr.refresh()
 
     curses.echo()
